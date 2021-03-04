41828e08766990f0f6b044c4a949be42dd475f46
Oliver Heckmann <oliver.heckmann@gmail.com>
2021-03-04 02:48:18 +0000

    integrated scd30 sensor

--- sensors.py
+++ sensors.py
@@ -7,11 +7,20 @@ from gpiozero import Button
 from gpiozero import LED
 
 import thingspeak
-import codaconfig # contains TABLE_ID, DOC_ID, API_TOKEN, SENTIMENT_COLUMN_ID
-import config # contains CHANNEL and API_KEY
+import codaconfig  # contains TABLE_ID, DOC_ID, API_TOKEN, SENTIMENT_COLUMN_ID
+import config  # contains CHANNEL and API_KEY
+
+import board
+import busio
+import adafruit_scd30
+
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+import subprocess
+
 
 ###########
-### CODA 
+### CODA API (to be removed)
 ###########
 
 
@@ -34,20 +43,43 @@ def post_sentiment_coda(sentiment: int):
 
 
 ##################
+### SENSOR
+##################
+
+# Create the I2C interface.
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# Create the interface to the SCD30 sensor (CO2, Temperature, Humidity)
+scd = adafruit_scd30.SCD30(i2c)
+
+def sample_sensors():
+    ret = {}
+    ret["temperature_c"] = scd.temperature
+    ret["temperature_f"] = scd.temperature * 1.8 + 32
+    ret["co2"] = scd.CO2
+    ret["humidity"] = scd.relative_humidity
+    return ret
+
+
+##################
 ### THINGSPEAK
 ##################
 
 ch = thingspeak.Channel(config.CHANNEL, api_key=config.API_KEY)
 
 
-def post_thingspeak(temperature, sentiment=-1):
-    print("posting %s and %s" % (temperature, sentiment))
-    ch.update({1: temperature, 2: sentiment}) if sentiment >= 0 else ch.update({1: temperature})
+def post_thingspeak(sensor_reading, sentiment=None):
+    if sentiment:
+        print("Sentiment is %s" % sentiment)
+    else:
+        print("posting %s ppm" % sensor_reading["co2"])
 
-
-def sample_sensors():
-    temp = os.popen("vcgencmd measure_temp").readline()
-    return (temp.replace("temp=", ""))
+    data = {2: sensor_reading["temperature_c"], 3: sensor_reading["temperature_f"], 4: sensor_reading["humidity"] }
+    if sensor_reading["co2"] > 0:
+        data[1] = sensor_reading["co2"]
+    if sentiment:
+        data[5] = sentiment
+    ch.update(data)
 
 
 #################
@@ -55,26 +87,24 @@ def sample_sensors():
 #################
 
 class FlexButton(Button):
-    def __init__(self, pin, buttonvalue, on_short_press, on_long_press = None):
+    def __init__(self, pin, buttonvalue, on_short_press, on_long_press=None):
         Button.__init__(self, pin)
         self.buttonvalue = buttonvalue
         self.on_short_press = on_short_press
         self.on_long_press = on_long_press
         self.when_pressed = self.handle_press
- 
+
     def handle_press(self):
-        start_time=time()
-        diff=0
+        start_time = time()
+        diff = 0
 
         while self.is_active and (diff < 2):
-            diff=time()-start_time
+            diff = time() - start_time
 
-        if diff < 2 or self.on_long_press == None:
+        if diff < 2 or self.on_long_press is None:
             self.on_short_press(self)
         else:
             self.on_long_press(self)
-
-
 
 
 ####################
@@ -82,25 +112,26 @@ class FlexButton(Button):
 ####################
 
 class LEDs():
-    def __init__(self, red_pin, green_pin = None):
+    def __init__(self, red_pin, green_pin=None):
         self.red = LED(red_pin)
         self.green = None
         if green_pin:
-            self.green = LED(green_pin) 
+            self.green = LED(green_pin)
 
-    def signal_error(self, error_code): 
-        self.red.blink(1,.33,error_code)
+    def signal_error(self, error_code):
+        self.red.blink(1, .33, error_code)
 
     def signal_ok(self):
         if self.green:
-            self.green.blink(.33,n=2)
-        else: 
-            self.red.blink(.2,.2, n=2)
+            self.green.blink(.33, n=2)
+        else:
+            self.red.blink(.2, .2, n=2)
 
-    def signal_ready(self): 
+    def signal_ready(self):
         if self.green:
-            self.green.blink(.1, .2, n=3, background=True)
-        self.red.blink(.1, .2, n=3, background=True)
+            self.green.blink(.2, .2, n=3, background=True)
+        self.red.blink(.2, .2, n=3, background=True)
+
 
 ###################
 ### CONTROL FLOW
@@ -114,18 +145,20 @@ def sentiment_action(button):
         leds.signal_ok()
     except:
         leds.signal_error(5)
-        
-        
+
+
 from subprocess import call
+
 
 def shutdown_action(button):
     leds.signal_ready()
     call("sudo nohup shutdown -h now", shell=True)
 
+
 if __name__ == '__main__':
 
-    PINS = [12, 1, 24, 23, 14] # typically three or 5 buttons
-    leds = LEDs(26) # either one LED or two (red and green)
+    PINS = [26, 25, 24, 23, 22]  # typically three or 5 buttons
+    leds = LEDs(6, 5)  # either one LED or two (red and green)
 
     buttons = []
     i = 1
@@ -140,5 +173,4 @@ if __name__ == '__main__':
     while True:
         temp = sample_sensors()
         post_thingspeak(temp)
-        print("temp " + temp)
         sleep(30)
